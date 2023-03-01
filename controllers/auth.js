a8ef3cf97bbbf6eb9b2f38a0d1555c1ea89a88ca
THSPSS <sunyeongbang98@gmail.com>
2023-03-02 02:10:29 +0900

    -udated auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,9 +6,33 @@ import jwt from "jsonwebtoken";
 const query = util.promisify(db.query).bind(db);
 
 
+export const checkID = (req , res ) => {
+
+    const userId = req.body.id
+    let msg = "사용할 수 있는 이메일입니다."
+
+    const q = "SELECT COUNT(id) AS cnt FROM members WHERE id = ? "
+
+    db.query(q , userId)
+        .then(([rows, fields]) => {
+            if(rows[0].cnt !== 0){msg = "이미 가입된 이메일입니다."}
+            return res.status(200).json({rows, message: msg})
+        })
+        .catch((err) => {
+            console.log(err)
+            return res.status(500).send("An error occured while fetching id data")
+        })
+
+}
+
+
 export const register = (req , res ) => {
 
-    const q1 = "INSERT INTO members(id, pass , name ,level, point) values( ? , ? , ? , 9, 0)"
+    const q = "INSERT INTO members(id, pass , name ,level, point) values( ? , ? , ? , 9, 0)"
+
+    //Hash the password and create a user
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.pass , salt);
 
     const values = [
         req.body.id,
@@ -18,9 +42,9 @@ export const register = (req , res ) => {
 
     db.query(q , values) 
         .then(([rows,fields]) => {
-        
-            return res.status(200).json({rows, message: "id has been created"})
-            })
+            return res.status(201).json({rows , message: "user has been created successfully"});
+
+        })
         .catch((err) => {
             console.log(err);
             return res.status(500).send("An error accured while fetching user data")
@@ -32,25 +56,36 @@ export const register = (req , res ) => {
 
 
 export const login = (req, res) => {
+
+    let statusNum 
+    let message
+    let userInfo = null
+
     const q = "SELECT * FROM members WHERE id = ?";
 
     db.query(q, req.body.id)
         .then(([rows, fields]) => {
             if (!rows.length) {
-                return res.status(404).json("User not found!");
+                statusNum = 404
+                message = "User not found!"
+            } else {
+                const idPassWordCorrect = bcrypt.compareSync(req.body.pass, rows[0].pass);
+
+                if (!idPassWordCorrect) {
+                    statusNum = 400
+                    message = "Wrong username or password"
+                } else{
+                    const token = jwt.sign({ id: rows[0].id }, "jwtkeyforcheck");
+                    const { pass, ...other } = rows[0];
+
+                    statusNum = 200
+                    message = "successfully logged in"
+                    userInfo = other
+
+                    res.cookie("access_token", token, { httpOnly: true });
+                }
             }
-
-            const idPassWordCorrect = bcrypt.compareSync(req.body.pass, rows[0].pass);
-
-            if (!idPassWordCorrect) {
-                return res.status(400).json("Wrong username or password");
-            }
-
-            const token = jwt.sign({ id: rows[0].id }, "jwtkeyforcheck");
-            const { pass, ...other } = rows[0];
-
-            res.cookie("access_token", token, { httpOnly: true });
-            return res.status(200).json(other);
+            return res.status(statusNum).json({userInfo , message});
         })
         .catch((err) => {
             console.log(err);
--- routes/auths.js
+++ routes/auths.js
@@ -1,10 +1,12 @@
 import expresss from 'express'
-import { register , login } from '../controllers/auth.js';
+import { register , login, checkID } from '../controllers/auth.js';
 
 
 const router = expresss.Router();
 
+router.post("/checkid", checkID);
 router.post("/register", register);
 router.post("/login", login);
+
 
 export default router
