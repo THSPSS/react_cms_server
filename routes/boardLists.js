032b3383f87bc56a259aeab3cb933129e9565d28
THSPSS <sunyeongbang98@gmail.com>
2023-03-05 22:11:24 +0900

    -commit boardList.js

--- routes/boardLists.js
+++ routes/boardLists.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getBoardLists ,addBoardList, getBoardList, getRecentPost , deletePost} from "../controllers/boardList.js";
+import { getBoardLists ,addBoardList, getBoardList, getRecentPost , deletePost , updatePost} from "../controllers/boardList.js";
 
 const router = express.Router();
 
@@ -7,7 +7,8 @@ router.get("/",getBoardLists);
 router.get("/recent" , getRecentPost)
 router.get("/:num", getBoardList);
 router.post("/",addBoardList);
-router.delete("/:num" , deletePost)
+router.delete("/:num" , deletePost);
+router.update("/:num" , updatePost);
 
 
 
