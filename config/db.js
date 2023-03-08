56733d2a2ecdb113b792183577954efc40135503
THSPSS <sunyeongbang98@gmail.com>
2023-03-08 23:06:15 +0900

    - fix error in boardList.js

--- config/db.js
+++ config/db.js
@@ -1,24 +1,21 @@
 //"use strict"
 
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-
-
 //createPool saving source for using server
-//db config 
+//db config
 //not username but user
 const config = {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database : process.env.DB_DBNAME,
-    port : process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT,
 };
 //db connection
 const db = mysql.createPool(config).promise();
 
 export default db;
-
--- routes/boardLists.js
+++ routes/boardLists.js
@@ -1,23 +1,20 @@
 import express from "express";
-import { getBoardLists ,addBoardList, getBoardList, getRecentPost , deletePost , updatePost} from "../controllers/boardList.js";
+import {
+  getBoardLists,
+  addBoardList,
+  getBoardList,
+  getRecentPost,
+  deletePost,
+  updatePost,
+} from "../controllers/boardList.js";
 
 const router = express.Router();
 
-router.get("/",getBoardLists);
-router.get("/recent" , getRecentPost)
+router.get("/", getBoardLists);
+router.get("/recent", getRecentPost);
 router.get("/:num", getBoardList);
-router.post("/",addBoardList);
-router.delete("/:num" , deletePost);
-router.update("/:num" , updatePost);
+router.post("/", addBoardList);
+router.delete("/:num", deletePost);
+router.put("/:num", updatePost);
 
-
-
-export default router
-
-
-
-
-
-
-
-
+export default router;
